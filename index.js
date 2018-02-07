800adcdefb3db72dc8a161c7a3318d47e145d911
ZhangYan <zhangyan@highwho.com>
2018-02-07 17:55:13 +0800

    combine file upload in project

--- index.js
+++ index.js
@@ -2,16 +2,20 @@
 const http = require('http');
 const fs = require('fs');
 const path = require('path');
+const formidable = require('formidable');
+const util = require('util');
 
 const hostname = '127.0.0.1';
 const port = 3000;
+
+const processCWD = process.cwd();
 
 const getFiles = (path) => {
   return fs.readdirSync(path);
 }
 
 const getLANPath = (path) => {
-  return path.replace(__dirname, '');
+  return path.replace(processCWD, '');
 }
 
 const generateLI = (text, path) => {
@@ -32,32 +36,37 @@ const generateHTML = (absolutePath) => {
     <head>
       <title>掩耳</title>
       <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+      <style>
+        html, body {
+          color: gray;
+        }
+      </style>
     </head>
     <body>
       <ul>${filesLi}</ul>
+      <br />
+      <form method="post" action="" enctype='multipart/form-data'>
+        <label for="upload">上传文件</label>
+        <input id="upload" type="file" name="file"/>
+        <input type="submit" value="提交" />
+      </form>
     </body>
     </html>`;
   return filesHTML;
 }
 
-const server = http.createServer((req, res) => {
+const handleGetFileAndDir = (req, res) => {
   const { url } = req;
-  if (url === '/favicon.ico') {
-    res.statusCode = 204;
-    res.end();
-    return;
-  }
   const absolutePath = path.resolve(`.${url}`);
 
   if (!fs.existsSync(absolutePath)) {
     res.statusCode = 404;
-    res.end();
+    res.end('file or directory isn\'t exist');
     return;
   }
 
-  const fileStats = fs.statSync(absolutePath);
-
   // directory
+  const fileStats = fs.statSync(absolutePath);  
   if (fileStats.isDirectory()) {
     res.statusCode = 200;
     res.end(generateHTML(absolutePath));
@@ -76,6 +85,51 @@ const server = http.createServer((req, res) => {
       res.end();
     })
     .pipe(res);
+}
+
+const handleUploadFile = (req, res) => {
+  const { url } = req;
+  const targetDir = path.resolve(processCWD, `.${url}`);
+
+  const form = new formidable.IncomingForm();
+  form.encoding = 'utf-8';
+  form.uploadDir = targetDir;
+  form.keepExtensions = true;
+  form.parse(req, function(err, fields, files) {
+    res.writeHead(301, {
+      Location: url
+    });
+    res.end();
+
+    const now = Date.now();
+    const pathObj = path.parse(files.file.name);
+    const filename = `${pathObj.name}_${now}${pathObj.ext}`;
+
+    fs.rename(files.file.path, `${path.resolve(targetDir, filename)}`, function (err) {
+      if (err) {
+        console.log(err);
+      }
+    })
+  })
+}
+
+const server = http.createServer((req, res) => {
+  const { url } = req;
+  if (url === '/favicon.ico') {
+    res.statusCode = 204;
+    res.end();
+    return;
+  }
+
+  const method = req.method;
+  
+  if (method === 'GET') {
+    handleGetFileAndDir(req, res);
+  }
+
+  if (method === 'POST') {
+    handleUploadFile(req, res);
+  }
 })
 
 server.listen(port, hostname, () => {
